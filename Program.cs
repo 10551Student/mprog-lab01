18ad2699a6eca87b35ecb79a7fe7a914995171c7
artur Milewski <10551@student.puzim.edu.pl>
2021-11-29 12:10:49 +0100

    added new dotnet console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mprog_lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
