103525da77913ac38f4d6d02a5ff663fb7c1b954
artur Milewski <10551@student.puzim.edu.pl>
2021-11-29 12:52:56 +0100

    test

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace mprog_lab01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            for(int i = 0 ; i < 100 ; i++ ){
+                
+                Console.WriteLine(i);
+
+            }
         }
     }
 }
